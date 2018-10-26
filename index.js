916541427494eb133a068efca421df53d103c038
guofangchao <guofangchao@daling.com>
2018-10-26 19:26:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * 入口，提供不同的类型
+ */
+const debug = require("debug")("webcore");
+
+debug("进入入口")
+module.export = {}
---
+++ test/index.js
@@ -0,0 +1,4 @@
+/**
+ * 测试各种方式使用库
+ */
+const debug = require("debug")("test");
